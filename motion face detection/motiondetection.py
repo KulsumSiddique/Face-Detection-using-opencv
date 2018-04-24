8c147f60551ed14c176956d75da619595b0112f4
kulsum_siddique <kulsumsiddiquenova@gmail.com>
2018-04-24 10:01:09 +0600

    done with motion face detection

--- motion face detection/motiondetection.py
+++ motion face detection/motiondetection.py
@@ -12,7 +12,7 @@ cap = cv2.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
@@ -24,7 +24,7 @@ while 1:
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
-    cv2.imshow('img',img)
+    cv2.imshow("img",img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
